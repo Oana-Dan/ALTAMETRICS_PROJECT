684ffbab94c3c9899c46f7cc1d62ed1446e7d238
Oana Andreea Dan <odan@lenovo.com>
2023-10-24 00:21:06 +0300

    added seed data

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,3 +1,54 @@
 import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient();
+
+async function main() {
+  try {
+    const user1 = await prisma.user.create({
+      data: {
+        email: 'oanadan2k@yahoo.com',
+        name: 'Oana Dan',
+        password: 'pass',
+      },
+    });
+
+    const user2 = await prisma.user.create({
+      data: {
+        email: 'cghiurea@altametrics.com',
+        name: 'Cris Ghiurea',
+        password: 'pass',
+      },
+    });
+
+    const bill1 = await prisma.bill.create({
+      data: {
+        amount: 30,
+        details: 'Bill for 27/11/2023',
+        due_date: new Date('2023-11-27'),
+        userId: user1.id,
+      },
+    });
+
+    const bill2 = await prisma.bill.create({
+      data: {
+        amount: 30,
+        details: 'Bill for 28/11/2023',
+        due_date: new Date('2023-11-28'),
+        userId: user1.id,
+      },
+    });
+
+    const bill3 = await prisma.bill.create({
+      data: {
+        amount: 30,
+        details: 'Bill for 27/11/2023',
+        due_date: new Date('2023-11-27'),
+        userId: user2.id,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
